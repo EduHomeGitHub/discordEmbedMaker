738eea0ec478abd7dd6385b1b93ee8423f0f900c
aradleir <137732498+aradleir@users.noreply.github.com>
2024-06-30 21:44:20 +0300

    first

---
+++ builder.config.js
@@ -0,0 +1,43 @@
+
+// Options
+
+// URL options can override the options below.
+// Options set through the menu can override both until the page is refreshed.
+options = {
+    username: 'Discord Bot',
+    avatar: 'https://cdn.discordapp.com/embed/avatars/1.png',
+    verified: false,
+    noUser: false,
+    data: null,
+    guiTabs: ['author', 'description'],
+    useJsonEditor: false,
+    reverseColumns: false,
+    allowPlaceholders: false,
+    autoUpdateURL: false,
+    autoParams: false,
+    hideEditor: false,
+    hidePreview: false,
+    hideMenu: false,
+    single: false,
+    noMultiEmbedsOption: true,
+    sourceOption: false, // Display link to source code in menu.
+}
+
+// Default JSON object
+
+// json = {
+//     content: "Hello world",
+//     embed: {
+//         title: "A title",
+//         description: "A description",
+//     }
+// }
+
+
+// Write any code under the 'DOMContentLoaded' event to run after the page has loaded.
+addEventListener('DOMContentLoaded', () => {
+    // console.log('Hello 👋');
+
+    // Remove the colour picker
+    // document.querySelector('.colors').remove()
+})
